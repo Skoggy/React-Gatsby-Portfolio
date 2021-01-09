75d63488dbeb8d5fc2bebdb73a215c7978c39727
Chris Skogstad <chris.skogstad1@gmail.com>
2021-01-09 16:41:21 +1100

    initial site built

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+// // import dotenv from 'dotenv';
+// const dotenv = require('dotenv')
+
+// dotenv.config({ path: '.env' });
+
+
+// module.exports = {
+//   siteMetadata: {
+//     title: "Christopher's Portfolio",
+//   },
+//   plugins: [
+//     {
+//       resolve: "gatsby-source-sanity",
+//       options: {
+//         projectId: "ifg2gihu",
+//         dataset: "production",
+//         watchMode: true,
+//         token: process.env.SANITY_TOKEN,
+//       },
+//     },
+
+//   ],
+// };
